be6f10c50581c1d630fcd786a9a210fc5b5412e4
user <james060702@gmail.com>
2024-04-16 16:36:49 +0900

    Second Commit

--- Assets/02.Script/PlayerController.cs
+++ Assets/02.Script/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public SpriteRenderer SR;
     public Animator Animator;
     public float MoveSpeed;
+    public float JumpForce; 
     public float v;
 
     private void Start()
@@ -21,21 +22,28 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         MoveInput();
-
+        JumpInput();
     }
 
     public void MoveInput()
     {
         float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
 
         if (moveX < 0) SR.flipX = true;
         else if (moveX > 0) SR.flipX = false;
-
-        Vector3 movePos = new Vector3(moveX, moveY, 0);
+        
+        Vector3 movePos = new Vector3(moveX, 0, 0);
         Rigid2D.velocity = movePos * MoveSpeed;
 
         v = Rigid2D.velocity.magnitude;
         Animator.SetFloat("Velocity", v);
+    }
+
+    public void JumpInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {   
+            Rigid2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
     }
 }
