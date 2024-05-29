8267aa5c6bd82152b51d7eaea548de3249dd9d3b
SoChanYoung <james060702@gmail.com>
2024-05-29 17:29:53 +0900

    third commit

--- Assets/02.Script/PlayerController.cs
+++ Assets/02.Script/PlayerController.cs
@@ -26,21 +26,28 @@ public class PlayerController : MonoBehaviour
     public void MoveInput()
     {
         float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
 
         if (moveX < 0) SR.flipX = true;
         else if (moveX > 0) SR.flipX = false;
         
-        Vector3 movePos = new Vector3(moveX, moveY, 0);
+        Vector3 movePos = new Vector3(moveX, 0, 0);
         Rigid2D.velocity = movePos * MoveSpeed;
 
         v = Rigid2D.velocity.magnitude;
         Animator.SetFloat("Velocity", v);
     }
     
+    public void JumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+
     public void RollInput()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.C))
         {
             
         }
