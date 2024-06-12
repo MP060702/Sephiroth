da69266d30584157d5dd9abb334d65eef672c190
SoChanYoung <james060702@gmail.com>
2024-06-13 01:24:14 +0900

    five commit

--- Assets/02.Script/PlayerController.cs
+++ Assets/02.Script/PlayerController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,6 +9,9 @@ public class PlayerController : MonoBehaviour
     public Animator Animator;
     public float MoveSpeed;
     public float JumpPower;
+    public bool isGround;
+    public float RollPower;
+    public float RollTime;
 
     private void Start()
     {
@@ -22,6 +24,7 @@ public class PlayerController : MonoBehaviour
     {
         MoveInput();
         JumpInput();
+        RollInput();
     }
 
     public void MoveInput()
@@ -30,28 +33,48 @@ public class PlayerController : MonoBehaviour
 
         if (moveX < 0) SR.flipX = true;
         else if (moveX > 0) SR.flipX = false;
-        
-        Vector3 movePos = new Vector3(moveX, 0, 0);
-        Rigid2D.velocity = movePos * MoveSpeed;
+
+        Vector3 movePos = new Vector3(moveX * MoveSpeed, Rigid2D.velocity.y, 0);
+        Rigid2D.velocity = movePos;
 
         Animator.SetFloat("xVelocity", Mathf.Abs(Rigid2D.velocity.x));
         Animator.SetFloat("yVelocity", Rigid2D.velocity.y);
     }
-    
+
     public void JumpInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
-            Rigid2D.velocity = new Vector2(Rigid2D.velocity.x, JumpPower);
-            Animator.SetBool("isJump", true);
+            Rigid2D.velocity = new Vector2(Rigid2D.velocity.x, JumpPower);;
+            Animator.SetBool("isJump", isGround);
+            isGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+            Animator.SetBool("isJump", false);
         }
     }
 
     public void RollInput()
     {
-        if(Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            
+            StartCoroutine(Roll());
         }
     }
+
+    private IEnumerator Roll()
+    {
+        float gravity = Rigid2D.gravityScale;
+        Rigid2D.gravityScale = 0f;
+        Rigid2D.velocity = new Vector2(transform.localScale.x * RollPower, 0f);
+        yield return new WaitForSeconds(RollTime);
+        Rigid2D.gravityScale = gravity;
+    }
+   
 }
